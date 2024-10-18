8680ba934ec2b0c657241cfb827cd84204d96d2d
Gjermund Garaba <gjermund@garaba.net>
2024-10-18 15:43:55 -0700

    neo-tree fixes

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -38,96 +38,78 @@ return {
   deactivate = function()
     vim.cmd [[Neotree close]]
   end,
-  init = function()
-    -- FIX: use `autocmd` for lazy-loading neo-tree instead of directly requiring it,
-    -- because `cwd` is not set up properly.
-    vim.api.nvim_create_autocmd('BufEnter', {
-      group = vim.api.nvim_create_augroup('Neotree_start_directory', { clear = true }),
-      desc = 'Start Neo-tree with directory',
-      once = true,
-      callback = function()
-        if package.loaded['neo-tree'] then
-          return
-        else
-          local stats = vim.uv.fs_stat(vim.fn.argv(0))
-          if stats and stats.type == 'directory' then
-            require 'neo-tree'
-          end
-        end
-      end,
-    })
-  end,
-  opts = {
-    sources = { 'filesystem', 'buffers', 'git_status' },
-    open_files_do_not_replace_types = { 'terminal', 'Trouble', 'trouble', 'qf', 'Outline' },
-    filesystem = {
-      bind_to_cwd = false,
-      follow_current_file = { enabled = true },
-      use_libuv_file_watcher = true,
-    },
-    window = {
-      mappings = {
-        ['l'] = 'open',
-        ['h'] = 'close_node',
-        ['<space>'] = 'none',
-        ['Y'] = {
-          function(state)
-            local node = state.tree:get_node()
-            local path = node:get_id()
-            vim.fn.setreg('+', path, 'c')
-          end,
-          desc = 'Copy Path to Clipboard',
-        },
-        ['O'] = {
-          function(state)
-            require('lazy.util').open(state.tree:get_node().path, { system = true })
-          end,
-          desc = 'Open with System Application',
-        },
-        ['P'] = { 'toggle_preview', config = { use_float = false } },
-      },
-    },
-    default_component_configs = {
-      indent = {
-        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
-        expander_collapsed = '',
-        expander_expanded = '',
-        expander_highlight = 'NeoTreeExpander',
-      },
-      git_status = {
-        symbols = {
-          unstaged = '󰄱',
-          staged = '󰱒',
-        },
-      },
-    },
-  },
-  config = function(_, opts)
-    --local function on_move(data)
-    --  LazyVim.lsp.on_rename(data.source, data.destination)
-    --end
+  config = function()
+    -- If you want icons for diagnostic errors, you'll need to define them somewhere:
+    vim.fn.sign_define('DiagnosticSignError', { text = ' ', texthl = 'DiagnosticSignError' })
+    vim.fn.sign_define('DiagnosticSignWarn', { text = ' ', texthl = 'DiagnosticSignWarn' })
+    vim.fn.sign_define('DiagnosticSignInfo', { text = ' ', texthl = 'DiagnosticSignInfo' })
+    vim.fn.sign_define('DiagnosticSignHint', { text = '󰌵', texthl = 'DiagnosticSignHint' })
 
-    --local events = require("neo-tree.events")
-    --opts.event_handlers = opts.event_handlers or {}
-    --vim.list_extend(opts.event_handlers, {
-    --  { event = events.FILE_MOVED, handler = on_move },
-    --  { event = events.FILE_RENAMED, handler = on_move },
-    --})
-    require('neo-tree').setup(opts)
-    vim.api.nvim_create_autocmd('User', {
-      pattern = 'PersistenceSavePre',
-      callback = function()
-        require('neo-tree.command').execute { action = 'close' }
-      end,
-    })
-
-    vim.api.nvim_create_autocmd('TermClose', {
-      pattern = '*lazygit',
-      callback = function()
-        if package.loaded['neo-tree.sources.git_status'] then
-          require('neo-tree.sources.git_status').refresh()
-        end
-      end,
-    })
+    require('neo-tree').setup {
+      filesystem = {
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+      },
+      window = {
+        mappings = {
+          ['l'] = 'open',
+          ['h'] = 'close_node',
+        },
+      },
+      default_component_configs = {
+        git_status = {
+          symbols = {
+            unstaged = '󰄱',
+            staged = '󰱒',
+          },
+        },
+      },
+    }
   end,
+
+  -- opts = {
+  --   sources = { 'filesystem', 'buffers', 'git_status' },
+  --   open_files_do_not_replace_types = { 'terminal', 'Trouble', 'trouble', 'qf', 'Outline' },
+  --   filesystem = {
+  --     bind_to_cwd = false,
+  --     follow_current_file = { enabled = true },
+  --     use_libuv_file_watcher = true,
+  --   },
+  --   window = {
+  --     mappings = {
+  --       ['l'] = 'open',
+  --       ['h'] = 'close_node',
+  --       ['<space>'] = 'none',
+  --       ['Y'] = {
+  --         function(state)
+  --           local node = state.tree:get_node()
+  --           local path = node:get_id()
+  --           vim.fn.setreg('+', path, 'c')
+  --         end,
+  --         desc = 'Copy Path to Clipboard',
+  --       },
+  --       ['O'] = {
+  --         function(state)
+  --           require('lazy.util').open(state.tree:get_node().path, { system = true })
+  --         end,
+  --         desc = 'Open with System Application',
+  --       },
+  --       ['P'] = { 'toggle_preview', config = { use_float = false } },
+  --     },
+  --   },
+  --   default_component_configs = {
+  --     indent = {
+  --       with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+  --       expander_collapsed = '',
+  --       expander_expanded = '',
+  --       expander_highlight = 'NeoTreeExpander',
+  --     },
+  --     git_status = {
+  --       symbols = {
+  --         unstaged = '󰄱',
+  --         staged = '󰱒',
+  --       },
+  --     },
+  --   },
+  -- },
 }
